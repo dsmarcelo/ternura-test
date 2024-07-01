8aa6f809f6ea0d489895c984b811a2994ae9ffd8
dsmarcelo <celoygm@gmail.com>
2024-07-01 15:22:07 -0300

    style: cart button styling

--- components/cart/open-cart.tsx
+++ components/cart/open-cart.tsx
@@ -9,14 +9,16 @@ export default function OpenCart({
   quantity?: number;
 }) {
   return (
-    <div className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors dark:border-neutral-700 dark:text-white">
+    <div className="relative flex h-11 w-11 items-center justify-center rounded-md border border-neutral-200 text-black transition-colors hover:bg-primary-100">
       <ShoppingCartIcon
         className={clsx('h-4 transition-all ease-in-out hover:scale-110 ', className)}
       />
 
       {quantity ? (
-        <div className="absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded bg-blue-600 text-[11px] font-medium text-white">
-          {quantity}
+        <div className="absolute right-0 top-0 -mr-2 -mt-2 h-4 w-4 rounded  bg-primary-500 text-[11px] font-medium text-white">
+          <p className={clsx("text-black")}>
+            {quantity}
+          </p>
         </div>
       ) : null}
     </div>
