c3d20a24da485ce3224b390c0668210291ec0d7f
dsmarcelo <celoygm@gmail.com>
2024-07-06 19:43:50 -0300

    feat: add product card in carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,6 +1,5 @@
 import { getCollectionProducts } from 'lib/shopify';
-import Link from 'next/link';
-import { GridTileImage } from './grid/tile';
+import ProductCard from './product/product-card';
 
 export async function Carousel() {
   // Collections that start with `hidden-*` are hidden from the search page.
@@ -16,26 +15,11 @@ export async function Carousel() {
     <div className=" w-full overflow-x-auto pb-6 pt-1">
       <ul className="flex animate-carousel gap-4">
         {carouselProducts.map((product, i) => (
-          <li
-            key={`${product.handle}${i}`}
-            className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
-          >
-            <Link href={`/produto/${product.handle}`} className="relative h-full w-full">
-              <GridTileImage
-                alt={product.title}
-                label={{
-                  title: product.title,
-                  amount: product.priceRange.maxVariantPrice.amount,
-                  currencyCode: product.priceRange.maxVariantPrice.currencyCode
-                }}
-                src={product.featuredImage?.url}
-                fill
-                sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
-              />
-            </Link>
+          <li key={`${product.handle}${i}`} className="md:w-48 lg:w-60">
+            <ProductCard product={product} />
           </li>
         ))}
       </ul>
-    </div>
+    </div >
   );
 }
